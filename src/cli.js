4a0ffd93366c1c62ce045cb49dff26c73a2847c1
Ash Wilson <smashwilson@github.com>
2017-09-08 14:27:55 -0400

    Normalize events to the @atom/watcher spec

--- src/cli.js
+++ src/cli.js
@@ -33,42 +33,13 @@ module.exports = async function (roots, facade, opts) {
 function reportEvents (events) {
   console.log('\n>> EVENT BATCH <<'.banner)
   for (const event of events) {
-    const normalized = {}
-
-    if (event.action) {
-      normalized.actionName = actionName(event.action)
-    } else if (event.type) {
-      normalized.actionName = event.type
-    }
-
-    if (event.kind) {
-      normalized.kind = event.kind
+    let output = `${event.action}:`.header
+    output += ` ${event.kind} `
+    if (event.oldPath) {
+      output += `(${event.oldPath} => ${event.path})`
     } else {
-      normalized.kind = 'unknown'
+      output += event.path
     }
-
-    if (event.directory) {
-      normalized.directory = event.directory
-    }
-
-    if (event.file) {
-      normalized.fileName = event.file
-    } else if (event.oldFile && event.newFile) {
-      normalized.fileName = `{${event.oldFile} => ${event.newFile}}`
-    } else if (event.oldPath && event.newPath === '') {
-      normalized.fileName = event.oldPath
-    } else if (event.oldPath && event.newPath) {
-      normalized.fileName = `{${event.oldPath} => ${event.newPath}}`
-    }
-
-    const fileName = event.file || `{${event.oldFile} => ${event.newFile}}`
-
-    let output = `${normalized.actionName}:`.header
-    output += ' '
-    if (normalized.directory) {
-      output += ` ${normalized.directory}${path.sep}`.sidenote
-    }
-    output += normalized.fileName.cyan
     console.log(output)
   }
 }
--- src/facade.js
+++ src/facade.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const watcher = require('@atom/watcher')
 const nsfw = require('nsfw')
 
@@ -11,11 +13,25 @@ class WatcherFacade {
   }
 }
 
+const NSFW_ACTIONS = new Map([
+  [nsfw.actions.MODIFIED, 'modified'],
+  [nsfw.actions.CREATED, 'created'],
+  [nsfw.actions.DELETED, 'deleted'],
+  [nsfw.actions.RENAMED, 'renamed']
+])
+
 class NsfwFacade {
   async start (rootDir, callback) {
     const watcher = await nsfw(
       rootDir,
-      events => { callback(null, events) },
+      events => callback(null, events.map(event => {
+        return {
+          action: NSFW_ACTIONS.get(event.action) || `unknown ${event.action}`,
+          kind: 'unknown',
+          oldPath: event.oldFile ? path.join(event.directory, event.oldFile) : undefined,
+          path: path.join(event.directory, event.file || event.newFile)
+        }
+      })),
       {debounceMS: 1, errorCallback: err => { callback(err) } }
     )
     await watcher.start()
