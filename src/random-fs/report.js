0beb8adb42e595f131deb402ceca48652379499e
Ash Wilson <smashwilson@github.com>
2017-10-26 15:43:25 -0400

    Collect a Report of a watcher's overall accuracy

---
+++ src/random-fs/report.js
@@ -0,0 +1,72 @@
+class Report {
+  constructor () {
+    this.created = {
+      exact: 0,
+      unknown: 0,
+      unexpected: 0
+    }
+
+    this.modified = {
+      exact: 0,
+      unknown: 0,
+      unexpected: 0
+    }
+
+    this.deleted = {
+      exact: 0,
+      unknown: 0,
+      unexpected: 0
+    }
+
+    this.renamed = {
+      exact: 0,
+      unknown: 0,
+      splitExact: 0,
+      splitHalfUnknown: 0,
+      splitFullUnknown: 0
+    }
+  }
+
+  count (match) {
+    function increment (counters) {
+      if (match.isExact()) counters.exact++
+      if (match.isUnknown()) counters.unknown++
+      if (match.isUnexpected()) counters.unexpected++
+      if (match.isSplitExact()) counters.splitExact++
+      if (match.isSplitHalfUnknown()) counters.splitHalfUnknown++
+      if (match.isSplitFullUnknown()) counters.splitFullUnknown++
+    }
+
+    match.when({
+      created: () => increment(this.created),
+      modified: () => increment(this.modified),
+      deleted: () => increment(this.deleted),
+      renamed: () => increment(this.renamed)
+    })
+  }
+
+  summarize () {
+    function total (counters) {
+      return Object.keys(counters).reduce((sum, key) => sum + counters[key], 0)
+    }
+
+    function section (name, counters) {
+      const t = total(counters)
+
+      console.log(`${name.header} total: ${t}`)
+      for (const key of Object.keys(counters)) {
+        const decamel = key.replace(/([a-z])([A-Z])/, (m, p1, p2) => p1 + ' ' + p2.toLowerCase())
+        const percent = Math.floor((counters[key] / t) * 10000) / 100
+
+        console.log(` - ${decamel}: ${counters[key]} ` + `(${percent}%)`.sidenote)
+      }
+    }
+
+    console.log('>> ACCURACY <<'.banner)
+    for (const key of ['created', 'modified', 'deleted', 'renamed']) {
+      section(key, this[key])
+    }
+  }
+}
+
+module.exports = {Report}
