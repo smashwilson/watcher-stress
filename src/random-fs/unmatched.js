69e23b077dd7c5dc8342dcdd2bdbaffc11e673c0
Ash Wilson <smashwilson@github.com>
2017-11-01 10:38:41 -0400

    No need for a doubly-linked-list I guess. Ah well

--- src/random-fs/unmatched.js
+++ src/random-fs/unmatched.js
@@ -2,32 +2,17 @@ const {Match} = require('./matcher')
 
 class Unmatched {
   constructor () {
-    this.head = null
-    this.tail = null
-
     this.byPath = new Map()
   }
 
   expect (matcher) {
-    const node = {matcher, prev: null, next: null}
-
-    if (!this.head) {
-      this.head = node
-      this.tail = node
-    } else {
-      this.tail.next = node
-      node.prev = this.tail
-
-      this.tail = node
-    }
-
     for (const p of matcher.getPaths()) {
       let ms = this.byPath.get(p)
       if (!ms) {
         ms = new Set()
         this.byPath.set(p, ms)
       }
-      ms.add(node)
+      ms.add(matcher)
     }
   }
 
@@ -35,23 +20,18 @@ class Unmatched {
     const potential = new Set()
     for (const p of [evt.path, evt.oldPath]) {
       if (!p) continue
-      for (const node of this.byPath.get(p) || []) {
-        potential.add(node)
+      for (const matcher of this.byPath.get(p) || []) {
+        potential.add(matcher)
       }
     }
 
-    for (const node of potential) {
-      const m = node.matcher.matches(evt)
+    for (const matcher of potential) {
+      const m = matcher.matches(evt)
       if (m) {
         // Remove this node
-        if (node.prev) node.prev.next = node.next
-        if (node.next) node.next.prev = node.prev
-        if (this.tail === node) this.tail = node.prev
-        if (this.head === node) this.head = node.next
-
-        for (const p of node.matcher.getPaths()) {
+        for (const p of matcher.getPaths()) {
           const ms = this.byPath.get(p)
-          if (ms) ms.delete(node)
+          if (ms) ms.delete(matcher)
         }
 
         return m
